e8e561c6fb8390323a4a16b7f61d7d621657c8d3
sachio <sachio.engineer@gmail.com>
2021-03-18 22:32:14 +0900

    :sparkles: Add Sessionsコントローラーとview(new)作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root 'pictures#index'
   resources :pictures
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 end
