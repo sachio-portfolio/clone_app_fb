ff7b5a1d96806d440837d3d6f735fcb9805e9db4
sachio <sachio.engineer@gmail.com>
2021-03-19 00:39:40 +0900

    :sparkles: Add Sessionsのdestroyアクション作成

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,16 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
       redirect_to pictures_path
     else
       flash.now[:danger] = "ログインに失敗しました"
       render :new
     end
+  end
+  def destroy
+    session.delete(:user_id)
+    flash.now[:notice] = "ログアウトしました"
+    redirect_to new_session_path
   end
 end
